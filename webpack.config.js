2c1d8de9ae2850f4046a446b120e6ecbb02ad0c8
Dilshan Nayanamadhu Liyanage <48806985+dilshanliyanage1000@users.noreply.github.com>
2024-03-28 20:51:50 -0400

    Updated webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "https://cute-daffodil-40015f.netlify.app/",
+    publicPath: "https://dilshanliyanage-studentapp2.netlify.app/",
   },
 
   resolve: {
@@ -44,7 +44,7 @@ module.exports = (_, argv) => ({
       name: "StudentApp2",
       filename: "remoteEntry.js",
       remotes: {
-        "StudentApp1": "StudentApp1@https://aquamarine-crisp-9e0299.netlify.app/remoteEntry.js"
+        "StudentApp1": "StudentApp1@https://dilshanliyanage-studentapp1.netlify.app/remoteEntry.js"
       },
       exposes: {},
       shared: {
