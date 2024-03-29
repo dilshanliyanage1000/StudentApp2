c67348c04d832556d024be1e15a6cd93ba037839
Dilshan Nayanamadhu Liyanage <48806985+dilshanliyanage1000@users.noreply.github.com>
2024-03-28 21:21:18 -0400

    Updated files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar() {
 
     return (
-        <nav className="navbar bg-primary navbar-expand-lg bg-body-tertiary fixed-top" data-bs-theme="dark">
+        <nav className="navbar navbar-expand-lg fixed-top" style={{ backgroundColor: "#26A69A" }} data-bs-theme="dark">
             <div className="container-fluid">
                 <a href="#" className={`navbar-brand`} to="/">Student Application #2</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
